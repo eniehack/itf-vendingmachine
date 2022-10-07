f44d5974f896bd2ad6c5404b8ae5dcfdb92fd751
Nakaya <eniehack@outlook.jp>
2022-10-08 02:07:58 +0900

    feature: cache data on localStorage

--- src/lib/vendingMachine.ts
+++ src/lib/vendingMachine.ts
@@ -1,4 +1,5 @@
 import { LatLng } from "leaflet";
+import { readable, type Readable } from "svelte/store";
 
 export class VendingMachine {
     private tags: Map<string, string>;
@@ -8,7 +9,8 @@ export class VendingMachine {
    constructor(elem: Object) {
        this.tags = new Map(Object.entries(elem["tags"]));
        this.lat = elem["lat"];
-       this.lng = elem["lng"];
+       this.lng = elem["lon"];
+       console.log(this.tags);
    }
 
     getPosition(): LatLng{
@@ -28,4 +30,66 @@ export class VendingMachine {
     getVending(): string {
         return this.tags.get("vending");
     }
+
+    toObject(): Object {
+        let out = {};
+        let tags = {};
+        for (let [k, v] of this.tags) tags[k] = v;
+        out["lat"] = this.lat;
+        out["lon"] = this.lng;
+        out["tags"] = tags;
+
+        return out;
+    }
 }
+
+export const vendingmachine: Readable<VendingMachine[]> = readable(undefined, async (set) => {
+  const query: string = "[out:json][timeout:25]; \
+way(id:183555030); \
+map_to_area-> .ulis; \
+way(id:183555029); \
+map_to_area -> .ut; \
+( \
+  node(area.ulis)[amenity=vending_machine]; \
+  node(area.ut)[amenity=vending_machine]; \
+); \
+out;";
+    const objToVendingMachine = (json : Object): VendingMachine[] => {
+        let arr = [];
+        json["elements"].forEach(elem => {
+            let m = new VendingMachine(elem);
+            arr.push(m);
+        });
+        return arr;
+    }
+
+    let vm = localStorage.getItem("vm");
+        if (vm !== null) {
+            try {
+                let json = objToVendingMachine(JSON.parse(vm));
+                set(json);
+            } catch (e) {
+                console.log(e);
+                window.alert("自動販売機データの取得に失敗しました\nリロードしてください");
+            }
+        } else {
+            let resp = await fetch("https://lz4.overpass-api.de/api/interpreter", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencode",
+                },
+                body: query,
+            });
+
+            if (resp.ok) {
+                let json = await resp.json()
+                set(objToVendingMachine(json));
+            } else {
+                let text = await resp.text();
+                window.alert("自動販売機データの取得に失敗しました\nリロードしてください");
+                throw new Error(text);
+            }
+        }
+    return () => {};
+});
