2a3f3f55b82fe2b907c84592f37691267462b1ab
Nakaya <32014963+eniehack@users.noreply.github.com>
2023-04-19 23:35:31 +0900

    feat: cache

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -23,13 +23,20 @@ out;';
 		mode: 'cors',
 		headers: {
 			'Content-Type': 'application/x-www-form-urlencode',
+			'Cache-Control': 'max-age=43200'
 		},
 		body: query
 	});
 
 	if (resp.ok) {
 		let json = await resp.json();
-		return json;
+		return {
+			status: 200,
+			body: json,
+			headers: {
+				'Cache-Control': 'max-age=43200, public' 
+			}
+		}
 	} else {
 		let text = await resp.text();
 		throw error(500, '自動販売機データの取得に失敗しました\nリロードしてください');
