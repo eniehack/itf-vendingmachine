6590eefbaeca5dc2a0afe274e182861b0c02f6de
Nakaya <eniehack@outlook.jp>
2023-08-24 13:36:54 +0900

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://itf-vendingmachine.vercel.app/",
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
