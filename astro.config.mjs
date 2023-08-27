9a8962d6872cc4d0c30d434f71bb3b174f2a401b
Nakaya <eniehack@outlook.jp>
2023-08-27 22:25:01 +0900

    fix: use edge inproduction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
-
+import vercelEdge from "@astrojs/vercel/edge";
 import sitemap from "@astrojs/sitemap";
 
+let config;
+if (import.meta.env.DEV) {
+  config = {
+    site: "http://127.0.0.1:3000/",
+    integrations: [svelte(), sitemap()],
+    adapter: vercel(),
+    output: 'server'
+  };
+} else {
+  config = {
+    site: "https://itf-vendingmachine.vercel.app/",
+    integrations: [ svelte(), sitemap()],
+    adapter: vercelEdge(),
+    output: 'server'
+  };
+}
+
 // https://astro.build/config
-export default defineConfig({
-  site: "https://itf-vendingmachine.vercel.app/",
-  integrations: [svelte(), sitemap()],
-  output: "server",
-  adapter: vercel()
-});
+export default defineConfig(config);
