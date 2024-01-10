1c6112c0e320c1d2dc52436152daf387ab9faa73
Nakaya <eniehack@outlook.jp>
2024-01-11 04:31:07 +0900

    feat: re-unuse edge function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,3 @@
-import type { Config } from '@sveltejs/adapter-vercel';
-
 export const ssr = true;
 export const csr = true;
 export const prerender = true;
-
-export const config: Config = {
-	runtime: 'edge'
-};
