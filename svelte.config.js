3de5600add29a46e02f899a35dde15aec3f5d47c
Nakaya <eniehack@outlook.jp>
2024-01-11 03:29:36 +0900

    feat: purgecss

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,24 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from "svelte-preprocess";
+import purgecss from "@fullhuman/postcss-purgecss";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: [
+		sveltePreprocess({
+			postcss: {
+				plugins: [
+					purgecss({
+						content: ['./src/**/*.html', './src/**/*.svelte'],
+    	    			whitelistPatterns: [/svelte-/],
+    	    			defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+					})
+				]
+			}
+		})
+	],
 
 	kit: {
 		adapter: adapter({
