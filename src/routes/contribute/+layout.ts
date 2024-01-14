843c21adf178be7524ec03e9eb36de49cc66d16a
Nakaya <eniehack@outlook.jp>
2024-01-14 12:24:12 +0900

    feat: prerender

--- src/routes/contribute/+layout.ts
+++ src/routes/contribute/+layout.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@sveltejs/adapter-vercel';
 
 export const ssr = true;
-//export const prerender = true;
+export const prerender = true;
 
 export const config: Config = {
 	runtime: 'nodejs18.x'
